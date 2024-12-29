efec7f73cda93c9e077cac812c198a7336799e8a
WilliamDevin23 <wdevinsp@gmail.com>
2024-12-29 15:11:31 +0700

    Directly copy instead script

--- script_for_media_upload/media_upload.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "Removing index-D57iKFts.js and index-D57iKFts.js.map"
-rm -rf node_modules/@strapi/upload/dist/_chunks/index-D57iKFts.js node_modules/@strapi/upload/dist/_chunks/index-D57iKFts.js.map
-echo "Copying index-D57iKFts.js and index-D57iKFts.js.map to _chunks"
-cp -R script_for_media_upload/scripts node_modules/@strapi/upload/dist/_chunks/
-mv node_modules/@strapi/upload/dist/_chunks/scripts node_modules/@strapi/upload/dist/_chunks
